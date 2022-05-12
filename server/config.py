8f81d70a16e8fca72483f32662d76bae8ed7fa26
LaxmanMaharjan <lxmnmrzn@gmail.com>
2022-05-12 18:47:12 +0545

    changing some configs

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgresql://postgres:@localhost/'
-database_name = 'auth_db'
+#postgres_local_base = 'postgresql://postgres:@localhost/'
+#database_name = 'auth_db'
 
 class BaseConfig:
     """Base configuration"""
@@ -14,6 +14,8 @@ class DevelopmentConfig(BaseConfig):
     """Development configuration."""
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
-    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
+    #SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
+
+    SQLALCHEMY_DATABASE_URI = 'postgres://bopntwngefzncl:cab0d33cd159244e6f2df23f9c84ed8625f1d72d7d4524582725ac3085864bfb@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d18sjh3u411v1h'
     JWKS = "/home/laxman/Projects/Python-and-Applications/Flask/jwt-auth/server/public/.well-known"
 
